c8f537cc871c25efadb48d7a9607021d5ff85e5e
Jarod Keene <jarod.keene@live.com>
2019-02-27 10:18:49 -0600

    added makefiles and defines

--- handler/help.h
+++ handler/help.h
@@ -1,6 +1,11 @@
+#ifndef HELP_H
+#define HELP_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 void help(void);
+
+#endif
--- handler/lfuncs.h
+++ handler/lfuncs.h
@@ -1,6 +1,10 @@
+#ifndef LFUNCS_H
+#define LFUNCS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 void lls(void);
+#endif
