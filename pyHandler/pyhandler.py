ca914ccf3a75a213690896c87bf9d2938f5e3a59
Jarod Keene <jarod.keene@live.com>
2019-03-31 13:52:33 -0500

    fixed bind so not just on localhost

--- pyHandler/pyhandler.py
+++ pyHandler/pyhandler.py
@@ -17,7 +17,7 @@ print("This is a handler for the one and only SneakyBeets payload")
 print("Written by Jarod Keene\n")
 
 
-HOST = '127.0.0.1'	# Standard loopback interface address (localhost)
+HOST = '0.0.0.1'		# Standard loopback interface address (localhost)
 PORT = 1234			# Port to listen on (non-privileged ports are > 1023)
 
 sessions = []
